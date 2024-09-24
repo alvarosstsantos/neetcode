454b753e2e4ebe2a0093dd519c40589dcd06f23b
Alvaro Santos <alvarosstsantos@gmail.com>
2024-09-24 19:52:52 -0300

    :tada: browser history

---
+++ design_browser_history.py
@@ -0,0 +1,92 @@
+class HistoryElement(object):
+
+    def __init__(self, url):
+        """
+        :type url: str
+        :type prev: HistoryElement
+        :type next: HistoryElement
+        """
+        self.url = url
+        self.prev = None
+        self.next = None
+
+
+class BrowserHistory(object):
+
+    def __init__(self, homepage):
+        """
+        :type homepage: str
+        """
+
+        self.curr = HistoryElement(homepage)
+
+        self.head = HistoryElement("")
+        self.tail = HistoryElement("")
+
+        self.curr.next = self.tail
+        self.curr.prev = self.head
+
+        self.head.next = self.curr
+        self.tail.prev = self.curr
+
+    def visit(self, url):
+        """
+        :type url: str
+        :rtype: None
+        """
+
+        node = HistoryElement(url)
+        node.prev = self.curr
+        node.next = self.tail
+        self.curr.next = node
+        self.curr = node
+
+    def back(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+
+        i = 0
+
+        while (self.curr.prev != self.head and i < steps):
+            i += 1
+            self.curr = self.curr.prev
+
+        return self.curr.url
+
+    def forward(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+
+        i = 0
+
+        while (self.curr.next != self.tail and i < steps):
+            i += 1
+            self.curr = self.curr.next
+
+        return self.curr.url
+
+
+if __name__ == "__main__":
+
+    history = BrowserHistory("leetcode.com")
+    assert history.back(0) == "leetcode.com"
+    assert history.back(1) == "leetcode.com"
+    assert history.back(2) == "leetcode.com"
+    assert history.forward(0) == "leetcode.com"
+    assert history.forward(1) == "leetcode.com"
+    assert history.forward(2) == "leetcode.com"
+    history.visit("google.com")
+    history.visit("facebook.com")
+    history.visit("youtube.com")
+    assert history.back(1) == "facebook.com"
+    assert history.back(1) == "google.com"
+    assert history.forward(1) == "facebook.com"
+    history.visit("linkedin.com")
+    assert history.forward(2) == "linkedin.com"
+    assert history.back(2) == "google.com"
+    assert history.back(7) == "leetcode.com"
+    assert history.forward(10) == "linkedin.com"
